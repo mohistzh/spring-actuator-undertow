a894b03bbe9054cda29483973a93c57fb1e040fb
Z <mohistzh@gmail.com>
2020-02-27 19:00:52 +0800

    Metrics facade

--- src/main/java/com/github/mohistzh/Application.java
+++ src/main/java/com/github/mohistzh/Application.java
@@ -2,12 +2,14 @@ package com.github.mohistzh;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 /**
  * @Author Jonathan
  * @Date 2020/2/27
  **/
 @SpringBootApplication
+@ComponentScan("com.github.mohistzh")
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
