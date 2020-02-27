bcc1c5ec213aa58482dd88e8027a903b44ac03b2
Z <mohistzh@gmail.com>
2020-02-27 18:26:53 +0800

    use embedded container Undertow instead of tomcat

---
+++ src/main/java/com/github/mohistzh/Application.java
@@ -0,0 +1,15 @@
+package com.github.mohistzh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author Jonathan
+ * @Date 2020/2/27
+ **/
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ src/main/java/com/github/mohistzh/controller/DemoController.java
@@ -0,0 +1,17 @@
+package com.github.mohistzh.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author Jonathan
+ * @Date 2020/2/27
+ **/
+@RestController
+public class DemoController {
+
+    @GetMapping(path = "/")
+    public String home() {
+        return "Welcome home";
+    }
+}
